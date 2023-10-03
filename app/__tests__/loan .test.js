ecf5bcc7a3dbec6a7da00c3956b777fdd6b5a25e
quakebob69 <Trad3rd@n>
2023-10-03 11:31:08 -0600

    init test setup (packages, config, jest, first test)

---
+++ app/__tests__/loan .test.js
@@ -0,0 +1,9 @@
+describe("loan", () => {
+    describe("get loan route", () => {
+        describe("given the loan does not exist", () => {
+            it("should return a 404", () => {
+                expect(true).toBe(true);
+            })
+        })
+    })
+})
