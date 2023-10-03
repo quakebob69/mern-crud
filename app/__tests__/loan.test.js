71c3a3d5a9b64d59ccae47a8d47e9e6d1ca09f2c
quakebob69 <Trad3rd@n>
2023-10-03 15:26:53 -0600

    test

--- app/__tests__/loan.test.js
+++ app/__tests__/loan.test.js
@@ -1,17 +1,27 @@
 const supertest = require('supertest')
-const asdf = require('./index.js');
-//const asdf = require("../controllers/loan.controller.js");
-
+const app = require('../../index');
 
 describe("loan", () => {
-    describe("get loan route", () => {
-        describe("given the loan does not exist", () => {
-            it("should return a 404", async () => {
-                const loanId = 'loan-123'
-                //await supertest().get('/api/loans/${loanId}').
-                //await supertest(asdf.app).get('http://localhost:3000/api/loans/1').
-                //expect(404);
-            })
-        })
+  describe("get loan route", () => {
+    describe("given the loan does not exist", () => {
+      it("should return a 404", async () => {
+        const loanId = 'product-123'
+        await supertest(app).get('/api/loans/${loanId}')
+          .expect(404);
+      })
     })
+  })
+
+  describe("given the loan does exist", () => {
+    it("should return a 200 status and the product", async () => {
+      //const product = await createProduct(productPayload);
+      const loanId = 1
+      const { body, statusCode } = await supertest(app).get(
+        `/api/loans/${loanId}`
+      );
+
+      expect(statusCode).toBe(200);
+      expect(body.loanId).toBe(loanId);
+    });
+  });
 })
