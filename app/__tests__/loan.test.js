de9670eb663283c4ab2300d474b93d8cef8f576f
quakebob69 <Trad3rd@n>
2023-10-03 19:50:14 -0600

    create loan (without borrowers) test

--- app/__tests__/loan.test.js
+++ app/__tests__/loan.test.js
@@ -3,6 +3,11 @@ const createServer = require('../utils/server')
 
 const app = createServer();
 
+const loanId = 456;
+const loanPayload = {
+  loanId: loanId,
+};
+
 describe("loan", () => {
   describe("get loan route", () => {
     describe("given the loan does not exist", () => {
@@ -14,8 +19,9 @@ describe("loan", () => {
     })
   })
 
+  /*
   describe("given the loan does exist", () => {
-    it("should return a 200 status and the product", async () => {
+    it("should return a 200 status and the load", async () => {
       //const product = await createProduct(productPayload);
       const loanId = 1
       const { body, statusCode } = await supertest(app).get(
@@ -25,5 +31,30 @@ describe("loan", () => {
       expect(statusCode).toBe(200);
       expect(body.loanId).toBe(loanId);
     });
+  });
+  */
+
+  it("should create the loan", async () => {
+    const { statusCode, body } = await supertest(app)
+      .post("/api/loans")
+      .send(loanPayload);
+
+    expect(statusCode).toBe(200);
+
+    /*
+    expect(body).toEqual({
+      __v: 0,
+      _id: expect.any(String),
+      createdAt: expect.any(String),
+      description:
+        "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
+      image: "https://i.imgur.com/QlRphfQ.jpg",
+      price: 879.99,
+      productId: expect.any(String),
+      title: "Canon EOS 1500D DSLR Camera with 18-55mm Lens",
+      updatedAt: expect.any(String),
+      user: expect.any(String),
+    });
+    */
   });
 })
